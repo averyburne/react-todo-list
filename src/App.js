322ebc0f5fed06c19aae84c80e8fff859f109e6a
Avery Burne <burneav@gmail.com>
2021-12-09 20:58:03 -0500

    App component successfully renders list
    App component allows use to input list item and maps it underneath form
    Trash can button also filters through list to remove item

--- src/App.js
+++ src/App.js
@@ -3,9 +3,45 @@ import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 
 function App() {
+const [inputValue, setInputValue] = useState('')
+const [toDos, setToDos] = useState([])
+
+const addToDo = (e) => {
+  e.preventDefault()
+  if (inputValue.trim() === '') {
+    return
+  }
+
+  setToDos([...toDos, {
+    text: inputValue,
+    id: uuidv4()
+  }])
+
+  setInputValue('')
+}
+
+const removeToDo = (id) => {
+  setToDos(toDos.filter(todo => todo.id !== id))
+}
+
   return (
     <div className="App">
-      <h1>Entry Please</h1>
+      <div className='container'>
+        <form onSubmit={addToDo}>
+          <input type="text"
+          placeholder="Add a task..."
+          value={inputValue}
+          onChange={e => setInputValue(e.target.value)}
+          />
+          <button type="submit">Add</button>
+        </form>
+        {toDos.map(todo => (
+          <div key={todo.id} className="todo">
+            <p>{todo.text}</p>
+            <i onClick={() => removeToDo(todo.id)} className="fas fa-trash"></i>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
